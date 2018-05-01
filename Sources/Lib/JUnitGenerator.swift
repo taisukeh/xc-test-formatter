056a8b576acd14348586f0823d529dbf6b69fc75
Taisuke Hori <taisuke.hori@gmail.com>
2018-05-01 18:05:12 +0900

    Fix

--- Sources/Lib/JUnitGenerator.swift
+++ Sources/Lib/JUnitGenerator.swift
@@ -25,19 +25,25 @@ public class JUnitGenerator: Generator {
 
   func testsuite(_ summary: TestableSummary) -> String {
     guard let test0 = summary.Tests[safe: 0],
-          let tests1 = test0.Subtests,
-          let test2 = tests1[safe: 0] else { return "" }
+          let tests0 = test0.Subtests,
+          let test1 = tests0[safe: 0],
+          let tests1 = test1.Subtests else { return "" }
+
+    let testcases = tests1.flatMap { test -> [String] in
+      guard let tests = test.Subtests else { return [] }
+      return tests.map { self.test(parentTest: test, test: $0) }
+    }.joined(separator: "\n")
 
     return """
       <testsuite id="\(summary.TargetName.xmlEscape)" name="\(summary.TestName.xmlEscape)" tests="\(summary.testSummary.tests)" failures="\(summary.testSummary.failed)" time="\(summary.Duration)">
-      \((test2.Subtests ?? []).map { test($0) }.joined(separator: "\n") )
+      \(testcases)
       </testsuite>
 """
   }
 
-  func test(_ test: Test) -> String {
+  func test(parentTest: Test, test: Test) -> String {
     return """
-      <testcase id="\(test.TestIdentifier.xmlEscape)" name="\(test.TestName.xmlEscape)" time="\(test.Duration)">
+      <testcase id="\(test.TestIdentifier.xmlEscape)" classname="\(parentTest.TestName)" name="\(test.TestName)" time="\(test.Duration)">
       \((test.FailureSummaries ?? []).map { failure($0) }.joined(separator: "\n"))
       </testcase>
 """
