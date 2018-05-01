d877652843be2039c42a31b92aa9c4bc6fd3cc07
Taisuke Hori <taisuke.hori@gmail.com>
2018-05-01 13:03:33 +0900

    Fix

--- Tests/LibTests/PlistParserTests.swift
+++ Tests/LibTests/PlistParserTests.swift
@@ -3,29 +3,27 @@ import Foundation
 @testable import Lib
 
 class PlistParserTests: XCTestCase {
-  let parser = PlistTestParser()
+  override func setUp() {
+    super.setUp()
+  }
 
-    override func setUp() {
-        super.setUp()
-    }
+  static var allTests = [
+    ("testParse", testParse),
+  ]
 
-    static var allTests = [
-        ("testParse", testParse),
-    ]
+  func testParse() throws {
+    let plist = "file:///Users/tahori/Library/Developer/Xcode/DerivedData/TestApp0-eokgthoeibfdbbczwrztpepdinxt//Logs/Test/5F749F97-27BC-4ACA-B201-9257FFD24B76_TestSummaries.plist"
+    // let plist = "file:///Users/tahori/workspace/xc-test-reporter/Tests/LibTests/fixtures/TestSummaries0.plist"
 
-    func testParse() throws {
-      let plist = "file:///Users/tahori/Library/Developer/Xcode/DerivedData/TestApp0-eokgthoeibfdbbczwrztpepdinxt//Logs/Test/5F749F97-27BC-4ACA-B201-9257FFD24B76_TestSummaries.plist"
-      // let plist = "file:///Users/tahori/workspace/xc-test-reporter/Tests/LibTests/fixtures/TestSummaries0.plist"
+    guard let url = URL(string: plist) else { XCTFail(); return }
 
-      guard let url = URL(string: plist) else { XCTFail(); return }
+    let data = try Data(contentsOf: url)
+    let decoder = PropertyListDecoder()
+    let r = try decoder.decode(Report.self, from: data)
 
-      let data = try Data(contentsOf: url)
-      let decoder = PropertyListDecoder()
-      let r = try decoder.decode(Report.self, from: data)
-
-      guard let outurl = URL(string: "file:///Users/tahori/workspace/xc-test-reporter/report") else { XCTFail(); return }
-      
-      try HtmlGenerator().generate(report: r, plistPath: url, outDir: outurl)
-      // XCTAssertEqual(PlistParser().parse(), 10)
-    }
+    guard let outurl = URL(string: "file:///Users/tahori/workspace/xc-test-reporter/report") else { XCTFail(); return }
+    
+    try HtmlGenerator().generate(report: r, plistPath: url, outDir: outurl)
+    // XCTAssertEqual(PlistParser().parse(), 10)
+  }
 }
